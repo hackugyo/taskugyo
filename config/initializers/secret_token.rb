d43f3cde9315a0e44104f536441c7fff3af86331
k-watanabe <k-watanabe@isp.co.jp>
2011-06-30 11:43:10 +0900

    rails new taskugyo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskugyo::Application.config.secret_token = 'd5d9ba2905cd5f37f830f7e1cd21888b59e821c4371f5e6896b81c3d253a3d83b3004ed899dc387661c1b95737c520427bbad3ab1b6d4240db14494e0367e447'
